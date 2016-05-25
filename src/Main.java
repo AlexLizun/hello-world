55b9c0b122c109d655fccb84e1227b1d54bd0d0b
Alex <laizon@rambler.ru>
2016-05-25 15:11:03 +0300

    Added Main file and method

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+/*
+ *.idea
+ /out
+  *.iml
+
+  * вот это в гитигнор :)
+  *
+  */
